18372c747cdebbc74620b434bd6e43f92afbe0f7
yunha <alr0604@gmail.com>
2019-12-04 14:42:38 +0900

    main page 추가

---
+++ expressServer.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const app = express()
+
+app.use(express.static(__dirname + '/public')); //디자인, 프론트앤드js, 이미지 파일 등 정적파일 저장
+app.set('views', __dirname + '/views'); //렌더링할 파일(html, ejs)은 views 폴더에 저장
+app.set('view engine', 'ejs');
+
+app.get('/', function (req, res) {
+    var greetingMsg = "HI"
+    res.send("<html><h1>Hello " + greetingMsg+ " World</h1></html>")
+})
+ 
+app.get('/main', function (req, res) {
+    res.render('main')
+})
+
+app.get('/prac', function (req, res) {
+    res.render('ejs')
+})
+
+app.get('/prac01', function (req, res) {
+    res.render('ejs01')
+})
+
+app.listen(3000)
--- requestExample.js
+++ requestExample.js
@@ -1,5 +1,5 @@
 var request = require('request');
-request('http://www.naver.com', function (error, response, body) {
+request('http://www.daum.net', function (error, response, body) {
   console.log('error:', error); // Print the error if one occurred
   console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
   console.log('body:', body); // Print the HTML for the Google homepage.
