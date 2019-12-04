72c062888f4f0c640e877e9fcce789e5f2fabece
yunha <alr0604@gmail.com>
2019-12-04 11:42:26 +0900

    practice

---
+++ nodejs/functions.js
@@ -0,0 +1,11 @@
+function plus(a, b) {
+    return a + b;
+}
+
+function minus(a, b) {
+    return a - b;
+}
+
+console.log(minus(10,1));
+
+console.log(plus(2,5));
---
+++ nodejs/object.js
@@ -0,0 +1,11 @@
+var car = {
+    name: "sonata",
+    ph: "500ph",
+    start: function(){
+        console.log("Starting car")
+    }
+}
+
+console.log(car.name);
+console.log(car.ph);
+car.start();
